1e4c26705bf00e88f8cbce0d6bdfa8470f735e35
Kerley de Sousa Santos <kerley_sousa@live.com>
2022-10-22 16:40:58 -0300

    Adequação as especificações

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ function start() {
 }
 
 function calculateImc(weight, height) {
+  return (weight / (height * height)).toFixed(2);
+}
+
+function ImcGroup(imc) {
   groups = [{min:16, max:16.99, message:'Muito abaixo do peso'},
             {min:17, max:18.49, message:'Abaixo do peso'},
             {min:18.5, max:24.99, message:'Peso normal'},
@@ -12,18 +16,20 @@ function calculateImc(weight, height) {
             {min:30, max:34.99, message:'Obesidade I'},
             {min:35, max:39.99, message:'Obesidade grau II'},
             {min:40, max:999.99, message:'Obesidade grau III'}];
-  const imc = weight / (height * height);
   const index = groups.findIndex(item => imc>=item.min && imc<=item.max);
-  return (index>=0) ? imc.toFixed(1) + ' - ' + groups[index].message : 'fora do intervalo previsto.';
+  return (index>=0) ? groups[index].message : 'Fora do intervalo previsto.';
 }
 
 function handleButtonClick() {
   var inputWeight = document.querySelector('#input-weight');
   var inputHeight = document.querySelector('#input-height');
   var imcResult = document.querySelector('#imc-result');
+  var imcGroup = document.querySelector('#imc-group');
   var weight = Number(inputWeight.value);
   var height = Number(inputHeight.value);
-  imcResult.innerHTML = calculateImc(weight, height);
+  imc = calculateImc(weight, height);
+  imcResult.innerHTML = imc;
+  imcGroup.innerHTML = ImcGroup(imc);
 }
 
 start();
